d2e807ee41f6e6c6399b185a2ba59a1b09cbe415
Daksh14 <41485688+Daksh14@users.noreply.github.com>
2026-02-03 12:00:41 -0500

    Change config file

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -5,28 +5,16 @@ const __dirname = fileURLToPath(new URL('.', import.meta.url));
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  transpilePackages: [
-    'website',
-    '@repo/cms-schema',
-    '@repo/supabase-utils',
-    '@repo/markdown-wasm',
-    '@auteur/trpc-utils',
-  ],
   serverExternalPackages: ['md4w'],
   images: {
     remotePatterns: [
       { protocol: 'https', hostname: '**' },
-      { protocol: 'http', hostname: 'localhost' },
+      { protocol: 'http', hostname: 'localhost' }
     ],
   },
   turbopack: {
-    root: resolve(__dirname, '../../..'),
-    resolveAlias: {
-      'website/lib/*': ['cms/apps/website/lib/*'],
-      'website/components/*': ['cms/apps/website/components/*'],
-      'website/server/*': ['cms/apps/website/server/*'],
-    },
-  },
+    root: resolve(__dirname, '../../..')
+  }
 };
 
 export default nextConfig;
